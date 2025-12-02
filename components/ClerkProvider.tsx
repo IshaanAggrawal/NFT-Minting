a9842edcdcc22d22540689520657e5a87f58145a
Ishaan Aggrawal <ishaanaggrawal101@gmail.com>
2025-12-02 10:39:21 +0530

    Fix middleware error: Remove next-themes dependency from ClerkProvider

--- components/ClerkProvider.tsx
+++ components/ClerkProvider.tsx
@@ -1,16 +1,13 @@
 'use client'
 
 import { ClerkProvider as ClerkReactProvider } from '@clerk/nextjs'
-import { useTheme } from 'next-themes'
 import { dark } from '@clerk/themes'
 
 export function ClerkProvider({ children }: { children: React.ReactNode }) {
-  const { resolvedTheme } = useTheme()
-  
   return (
     <ClerkReactProvider
       appearance={{
-        baseTheme: resolvedTheme === 'dark' ? dark : undefined,
+        baseTheme: dark,
         variables: {
           colorPrimary: '#9D4EDD',
         },
